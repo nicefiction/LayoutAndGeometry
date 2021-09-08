e0d9783c39729a33bd3675f3a6700ab66405324d
Olivier Van hamme <nicefiction@gmail.com>
2021-09-08 07:51:48 +0200

    Day 93.3 : Refactors the rotation3DEffect modifier of the Cover Flow View .

--- LayoutAndGeometry/Day 93/CoverFlowScrolling.swift
+++ LayoutAndGeometry/Day 93/CoverFlowScrolling.swift
@@ -10,6 +10,15 @@ import SwiftUI
 
 struct CoverFlowScrolling: View {
    
+   // MARK: - NESTED TYPES
+      
+      private struct Dimension {
+         
+         static let cardHeight: CGFloat = 150
+      }
+      
+      
+   
    // MARK: - PROPERTIES
    
    let colors: Array<Color> = [
@@ -26,26 +35,33 @@ struct CoverFlowScrolling: View {
             HStack {
                ForEach(0..<50) { index in
                   GeometryReader { innerGeometryProxy in
+                     
+                     let cardHalfWidth: CGFloat = innerGeometryProxy.frame(in: .global).midX
+                     let mainViewHalfWidth: CGFloat = outerGeometryProxy.size.width / 2
+                     let cardRotation: Double = -Double((cardHalfWidth - mainViewHalfWidth) / 10)
+                     
                      Rectangle()
                         .fill(colors[index % 7])
-                        .frame(height: 150)
-                        .rotation3DEffect(.degrees(-Double(innerGeometryProxy.frame(in: .global).midX - outerGeometryProxy.size.width / 2) / 10),
+                        .frame(height: Dimension.cardHeight)
+                        .rotation3DEffect(.degrees(cardRotation),
                                           axis: (x: 0, y: 1, z: 0))
                   }
-                  .frame(width: 150)
+                  .frame(width: Dimension.cardHeight)
                }
             }
             /// Positions the first square in the middle .
             .padding(.horizontal,
-                     (outerGeometryProxy.size.width - 150) / 2)
+                     (outerGeometryProxy.size.width - Dimension.cardHeight) / 2)
             /// Uncovers the edges of the rotated rectangles .
             .padding(.vertical)
          }
+         .frame(height: 300)
          .position(x: outerGeometryProxy.frame(in: .local).midX,
-                   y: outerGeometryProxy.frame(in: .local).midY * 1.75)
+                   y: outerGeometryProxy.frame(in: .local).midY)
       }
    }
 }
+//Int(geometry.frame(in: .local).minX)
 
 
 
